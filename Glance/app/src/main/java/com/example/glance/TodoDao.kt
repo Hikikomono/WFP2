592a603edac5af2581ac9fb6b73ceabc06ed8711
RoRo <enkoroberto@gmail.com>
2021-03-09 19:37:53 +0100

    updated TodoDao
    multiple Queries added

--- Glance/app/src/main/java/com/example/glance/TodoDao.kt
+++ Glance/app/src/main/java/com/example/glance/TodoDao.kt
@@ -1,6 +1,5 @@
 package com.example.glance
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -11,4 +10,24 @@ interface TodoDao {
     @Query("SELECT * FROM todo_table WHERE area = :area")
     fun getAllTodoFromArea(area : String) : Flow<List<Todo>>
 
+    @Query("SELECT * FROM todo_table WHERE area = :area AND completed = 'false'")
+    fun getAllTodoFromAreaNotCompleted(area: String) : Flow<List<Todo>>
+
+    @Query("SELECT * FROM todo_table WHERE area = :area AND completed = 'true'")
+    fun getAllTodoFromAreaCompleted(area: String) : Flow<List<Todo>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAll(todo: Todo)
+
+    @Update
+    fun updateTodo(todo: Todo)
+
+    @Query ("UPDATE todo_table SET completed = 'true' WHERE id = :id")
+    fun updateSetTodoCompleted(id: Int)
+
+    @Query ("UPDATE todo_table SET area = :newArea WHERE id = :id")
+    fun updateTodoArea(id: Int, newArea: String)
+
+    @Delete
+    fun deleteTodo(todo: Todo)
 }
