635008c18560fc9d86ecc9f2f9b1fd5ffe8fef1d
RoRo <enkoroberto@gmail.com>
2021-03-10 15:20:36 +0100

    BugFix: Build Error
    necessary dependency added

--- Glance/app/src/main/java/com/example/glance/Todo.kt
+++ Glance/app/src/main/java/com/example/glance/Todo.kt
@@ -10,9 +10,11 @@ class Todo(
         @ColumnInfo(name = "area", defaultValue = "inbox") val area : String,
         @ColumnInfo(name = "title") val title : String,
         @ColumnInfo(name = "description") val description : String?,
+        /* TODO Dateconverter implementieren -> dann Kommentierung von Properties entfernen
         @ColumnInfo(name = "created_date") val created_date : Date,
         @ColumnInfo(name = "deadline_date") val deadline_date : Date?,
         @ColumnInfo(name = "do_date") val do_date : Date?,
         @ColumnInfo(name = "completed_date") val completed_date : Date?,
+        */
         @ColumnInfo(name = "completed", defaultValue = "false") val completed : Boolean
 )
