67fa0585ee706683b5ad4f0d33f9c6c41a091aaf
Johannes Schwinger <joderpro28@gmail.com>
2021-03-13 18:27:23 +0100

    added code for topAppBar and navDrawer

--- Glance/app/src/main/java/com/example/glance/MainActivity.kt
+++ Glance/app/src/main/java/com/example/glance/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.glance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.glance.databinding.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: TopBarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.top_bar)
+        binding = DataBindingUtil.setContentView(this, R.layout.top_bar)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = host.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerView)
+        setSupportActionBar(binding.topAppBar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
