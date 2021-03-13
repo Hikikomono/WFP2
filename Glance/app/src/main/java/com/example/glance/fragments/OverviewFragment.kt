80cbe2a3c1de92586c0baafb0c6ff818a5461af3
RoRo <enkoroberto@gmail.com>
2021-03-13 14:25:04 +0100

    added Databinding to Layouts
    upgraded gradle to include databinding

--- Glance/app/src/main/java/com/example/glance/fragments/OverviewFragment.kt
+++ Glance/app/src/main/java/com/example/glance/fragments/OverviewFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.glance.R
+import com.example.glance.databinding.*
+
 
 class OverviewFragment : Fragment() {
 
@@ -15,5 +18,9 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.overview_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
