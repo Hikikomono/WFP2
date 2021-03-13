0c64aab4b082c2200bf56c06453f4f2ebb350757
RoRo <enkoroberto@gmail.com>
2021-03-13 18:28:32 +0100

    updatet Fragments clickListener for navigation
    added clickListeners to various Fragments (Overview, TodoList) in order to navigate between Screens

--- Glance/app/src/main/java/com/example/glance/fragments/OverviewFragment.kt
+++ Glance/app/src/main/java/com/example/glance/fragments/OverviewFragment.kt
@@ -6,21 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.glance.R
 import com.example.glance.databinding.*
 
 
 class OverviewFragment : Fragment() {
+    private lateinit var binding: OverviewScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.overview_screen, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.overview_screen, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        
+        //Navigate to EditTodoFragment
+        binding.floatingActionButton.setOnClickListener { view: View ->
+            this.findNavController().navigate(R.id.action_OverviewFragment_to_EditScreenFragment)
+        }
+
+        //Navigate to TodoListFragment
+        binding.inboxLayout.setOnClickListener { view1 : View ->
+            this.findNavController().navigate(R.id.action_OverviewFragment_to_TodoListFragment)
+        }
+
     }
 }
--- Glance/app/src/main/java/com/example/glance/fragments/TodoListFragment.kt
+++ Glance/app/src/main/java/com/example/glance/fragments/TodoListFragment.kt
@@ -1,19 +1,59 @@
 package com.example.glance.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.glance.R
+import com.example.glance.TodoApplication
+import com.example.glance.TodoListAdapter
+import com.example.glance.data.todo.TodoViewModel
+import com.example.glance.data.todo.TodoViewModelFactory
+import com.example.glance.databinding.*
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+
 
 class TodoListFragment : Fragment() {
+    private lateinit var binding: TodoListViewBinding
+    private val todoViewModel: TodoViewModel by viewModels {
+        TodoViewModelFactory((context?.applicationContext as TodoApplication).repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.todo_list_view, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.todo_list_view, container, false)
+
+        //Init RecylcerView
+        val recyclerView = binding.todosRecyclerView
+        val adapter = TodoListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        todoViewModel.allTodos.observe(viewLifecycleOwner, Observer {  todos ->
+            todos.let { adapter.submitList(it) }
+        } )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.floatingActionButton.setOnClickListener { view: View ->
+            this.findNavController().navigate(R.id.action_TodoListFragment_to_EditScreenFragment)
+        }
     }
 }
