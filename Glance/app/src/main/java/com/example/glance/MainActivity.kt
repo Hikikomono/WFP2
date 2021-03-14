fb5471a5322b3ae82439c212889d6764ab54cffd
Johannes Schwinger <joderpro28@gmail.com>
2021-03-14 17:51:53 +0100

    added navDrawer functionality

--- Glance/app/src/main/java/com/example/glance/MainActivity.kt
+++ Glance/app/src/main/java/com/example/glance/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.glance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -10,28 +13,59 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.glance.databinding.*
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: TopBarBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // setup binding
         binding = DataBindingUtil.setContentView(this, R.layout.top_bar)
 
+        // setup values
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
         val navController = host.navController
-
         val drawer = binding.drawerView
-
+        val navigationView: NavigationView = binding.navigationView
         appBarConfiguration = AppBarConfiguration(navController.graph, drawer)
+
+        // setup AppBar
         setSupportActionBar(binding.topAppBar)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        // setup navDrawer
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.InboxMenu -> {
+                Toast.makeText(this, "Inbox it is then", Toast.LENGTH_SHORT).show()
+            }
+            R.id.TodayMenu -> {
+                Toast.makeText(this, "Today it is then", Toast.LENGTH_SHORT).show()
+            }
+            R.id.HabitsMenu -> {
+                Toast.makeText(this, "Habits it is then", Toast.LENGTH_SHORT).show()
+            }
+            R.id.ScheduleMenu -> {
+                Toast.makeText(this, "Schedule it is then", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
 
 
     override fun onSupportNavigateUp(): Boolean {
